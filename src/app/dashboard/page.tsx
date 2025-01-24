c00364e3761a146c46a987aae15a4c4dc25dc492
Wellington Pinho <wellpinho@outlook.com>
2025-01-24 20:11:16 -0300

    layout and dashboard component

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { ContainerLayout } from "@/components/container";
+
+export default function Dashboard() {
+    return (
+        <ContainerLayout>
+            <h1>dashboard</h1>
+        </ContainerLayout>
+    );
+}
