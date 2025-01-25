20d0defb5f1ec067e2398277b2b85edca4915331
Wellington Pinho <wellpinho@outlook.com>
2025-01-25 00:49:58 -0300

    private routes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,17 @@
 import { ContainerLayout } from "@/components/container";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
-export default function Dashboard() {
+export default async function Dashboard() {
+    const session = await auth();
+
+    if (!session) {
+        redirect("/");
+    }
+
     return (
         <ContainerLayout>
-            <h1>dashboard</h1>
+            <pre>{JSON.stringify(session, null, 2)}</pre>
         </ContainerLayout>
     );
 }
